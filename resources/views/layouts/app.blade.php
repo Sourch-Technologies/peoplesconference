c99ed6923b584142140393b9029ff327d3ffd151
masoodimubashir <masudimubashir@gmail.com>
2023-11-18 14:39:00 +0530

    ..

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <body class="bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen ">
             
             @include('layouts.navigation')
 
