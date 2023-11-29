8fcf7f36b69e7744bd45ee0025f2a7cfd71adc83
mubashir <masudimubashir@gmail.com>
2023-11-29 21:43:17 +0530

    ..

--- resources/views/layouts/PollingStation/pollingStation.blade.php
+++ resources/views/layouts/PollingStation/pollingStation.blade.php
@@ -161,7 +161,7 @@
                                                                                     @foreach($pollingStation->sectionnames as $sectionname)
 
                                                                                                                                                                    <li>
-{{$sectionname->members_count}}
+                                                                                            {{$sectionname->members_count}}
                                                                                                                                                                    </li>
                                                                                     @endforeach
 
@@ -210,15 +210,9 @@
                                                 </div>
                                             </div>
                                         </div>
-
                                     </div>
-
-
                             </div>
                             {{ $pollingStations->links() }}
-
-
-
                         </div>
                     </div>
 
